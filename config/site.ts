03d284f28dc1a524d79e25ad54b7eb4c290e8b12
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-05-08 19:37:18 -0300

    Fix: Cambios en la ruta de las paginas

--- config/site.ts
+++ config/site.ts
@@ -6,16 +6,16 @@ export const siteConfig = {
         "Un portal web para la adopción y cuidado de animales es una plataforma virtual que facilita la conexión entre mascotas necesitadas de hogar y personas interesadas en brindarles amor y cuidados. Ofrece perfiles detallados de animales disponibles, información sobre cuidados básicos y salud, citas para visitar refugios, sistemas de donaciones y foros comunitarios. Promueve la adopción responsable y el bienestar animal.",
     navItems: [
         {
-            label: "Pricing",
-            href: "/pricing",
+            label: "Info",
+            href: "/info",
         },
         {
             label: "Blog",
             href: "/blog",
         },
         {
-            label: "About",
-            href: "/about",
+            label: "Adoptar",
+            href: "/adopt",
         },
     ],
     navMenuItems: [
@@ -24,16 +24,16 @@ export const siteConfig = {
             href: "/",
         },
         {
-            label: "Pricing",
-            href: "/pricing",
+            label: "Info",
+            href: "/info",
         },
         {
             label: "Blog",
             href: "/blog",
         },
         {
-            label: "About",
-            href: "/about",
+            label: "Adoptar",
+            href: "/adopt",
         },
     ],
     links: {
