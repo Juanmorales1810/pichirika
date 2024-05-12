c584c19aa175f183ccf59375e084bf8eacf95774
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-05-12 20:53:29 -0300

    Add: id cardpichirika

--- components/cardpichirika.tsx
+++ components/cardpichirika.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
 import { fontMono } from "@/config/fonts";
+import { Link } from "@nextui-org/link";
+
 
 interface CardPichiriKaProps {
+    id: number;
     title: string;
     fecha: string;
     image: string;
@@ -10,7 +13,7 @@ interface CardPichiriKaProps {
 }
 
 export default function CardPichiriKa(props: CardPichiriKaProps) {
-    const { title, fecha, image, ubication } = props;
+    const { title, fecha, image, ubication, id } = props;
     return (
         <article className="flex flex-col justify-center items-center gap-4 w-48 bg-lime-300 dark:bg-lime-800 rounded-2xl p-4 border-3 border-lime-700 dark:border-lime-400">
             <header>
@@ -22,7 +25,7 @@ export default function CardPichiriKa(props: CardPichiriKaProps) {
             </section>
             <footer className="flex justify-center items-center gap-2">
                 <small className="text-xs">{fecha}</small>
-                <Button className="font-semibold" variant="solid" color="success">Adoptar</Button>
+                <Button as={Link} href={`/adopt/${id}`} className="font-semibold" variant="solid" color="success">Adoptar</Button>
             </footer>
 
         </article>
