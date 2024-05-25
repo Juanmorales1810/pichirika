ca001ed1d4a25f3417e1e1d33bb36d38c6210499
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-05-25 19:15:22 -0300

    Feat: Se agregaron los componentes de las paginas

---
+++ app/guard/forgetpassword/page.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div>
+            <h1>ForgetPassword</h1>
+        </div>
+    )
+}
--- app/info/page.tsx
+++ app/info/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function PricingPage() {
 	return (
 		<div>
-			<h1 className={title()}>Pricing</h1>
+			<h1 className={title()}>Info</h1>
 		</div>
 	);
 }
