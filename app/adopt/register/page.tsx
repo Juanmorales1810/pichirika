3b4dca3dc08893f0f49c7dd85dc099f363b59c45
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-05-27 00:13:09 -0300

    Fix: Cambios en la UI

--- app/adopt/register/page.tsx
+++ app/adopt/register/page.tsx
@@ -11,6 +11,7 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { fontMono } from "@/config/fonts"
 import { useState } from "react";
+import { Chip } from "@nextui-org/chip";
 
 type Inputs = {
     name: string;
@@ -46,6 +47,9 @@ export default function Register() {
             <h1 className={"text-2xl py-4 font-extrabold " + fontMono.className}>Registro de animales en adopción</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center gap-2">
                 <Input
+                    classNames={{
+                        inputWrapper: "bg-lime-100 dark:bg-green-950 shadow-md",
+                    }}
                     id="name"
                     type="text"
                     placeholder="Nombre"
@@ -55,9 +59,9 @@ export default function Register() {
                     isInvalid={!!errors.name}
                     errorMessage={errors.name?.message}
                     endContent={
-                        <Button onClick={generateName} color="success" size="sm" className="text-xs text-zinc-950 font-semibold">
+                        <Chip onClick={generateName} className="bg-lime-300 text-xs text-zinc-950 font-semibold dark:text-zinc-400 dark:bg-green-900 cursor-pointer select-none">
                             Generar nombre
-                        </Button>
+                        </Chip>
                     }
                     onChange={(e) => {
                         setName(e.target.value);
@@ -65,6 +69,10 @@ export default function Register() {
                     }}
                 />
                 <Select
+                    classNames={{
+                        trigger: "bg-lime-100 dark:bg-green-950 shadow-md",
+                        popoverContent: "bg-lime-100 dark:bg-green-950",
+                    }}
                     id="department"
                     placeholder="Departamento"
                     {...register("department")}
@@ -79,6 +87,9 @@ export default function Register() {
                     ))}
                 </Select>
                 <Input
+                    classNames={{
+                        inputWrapper: "bg-lime-100 dark:bg-green-950 shadow-md",
+                    }}
                     id="street1"
                     type="text"
                     placeholder="Calle 1"
@@ -87,6 +98,9 @@ export default function Register() {
                     errorMessage={errors.street1?.message}
                 />
                 <Input
+                    classNames={{
+                        inputWrapper: "bg-lime-100 dark:bg-green-950 shadow-md",
+                    }}
                     id="street2"
                     type="text"
                     placeholder="Calle 2"
@@ -95,13 +109,16 @@ export default function Register() {
                     errorMessage={errors.street2?.message}
                 />
                 <Textarea
+                    classNames={{
+                        inputWrapper: "bg-lime-100 dark:bg-green-950 shadow-md",
+                    }}
                     id="description"
                     placeholder="Descripción"
                     {...register("description")}
                 />
                 <div className="flex flex-col w-full ">
                     <input
-                        className={`text-default-500 block w-full file:py-2 file:px-4 file:font-light file:rounded-lg file:border-0 file:border-solid ${errors.image ? "file:bg-danger-50 dark:file:bg-danger-50" : "file:bg-default-100"} file:outline-none file:transition-all file:duration-200 file:ease-in-out`}
+                        className={`text-default-500 block w-full file:py-2 file:px-4 file:font-light file:rounded-lg file:border-0 file:border-solid ${errors.image ? "file:bg-danger-50 dark:file:bg-danger-50" : "file:bg-lime-300 shadow-md rounded-xl bg-lime-100 dark:file:bg-green-900 dark:bg-green-950"} file:outline-none file:transition-all file:duration-200 file:ease-in-out`}
                         id="image"
                         accept="image/*;capture=camera"
                         type="file"
@@ -116,7 +133,7 @@ export default function Register() {
                 >
                     ¿Esta en situación de calle?
                 </Checkbox>
-                <Button color="success" className={"text-xl w-32 mt-4 text-zinc-950 font-bold " + fontMono.className} type="submit">Registrar</Button>
+                <Button color="success" className={"text-xl w-32 mt-4 bg-lime-300 text-zinc-950 dark:text-zinc-100 dark:bg-green-900 font-bold " + fontMono.className} type="submit">Registrar</Button>
             </form>
         </div>
     );
