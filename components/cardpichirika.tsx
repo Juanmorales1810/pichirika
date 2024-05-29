ef66920992eb698e75c37b8571a7a54624a02ce1
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-05-29 12:39:08 -0300

    Fix: Se corrigió el nombre y la interface del departamento

--- components/cardpichirika.tsx
+++ components/cardpichirika.tsx
@@ -1,6 +1,7 @@
+import { Department, mappedDepartment } from "@/validations/userSchema";
 import { Button } from "@nextui-org/button";
-import Image from "next/image";
 import { fontMono } from "@/config/fonts";
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -8,7 +9,7 @@ interface CardPichiriKaProps {
     id: string;
     title: string;
     image: string;
-    ubication: string;
+    ubication: Department;
 }
 
 export default function CardPichiriKa(props: CardPichiriKaProps) {
@@ -20,7 +21,7 @@ export default function CardPichiriKa(props: CardPichiriKaProps) {
             </header>
             <section className="text-center">
                 <h3 className={"text-2xl font-bold " + fontMono.className}>{title}</h3>
-                <p className="font-semibold">{ubication}</p>
+                <p className="font-semibold">{ubication in mappedDepartment ? mappedDepartment[ubication] : ubication}</p>
             </section>
             <footer className="flex justify-center items-center gap-2">
                 <Button as={Link} href={`/adopt/${id}`} className="font-semibold" color="success">Adoptar</Button>
