b9d61d523ee1b489efddbcfbac6d113d226cb4c3
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-01 16:19:29 -0300

    Fix: Se cambio el nombre del nava de info a veterinarias y sus rutas

--- config/site.ts
+++ config/site.ts
@@ -6,8 +6,8 @@ export const siteConfig = {
         "Un portal web para la adopción y cuidado de animales es una plataforma virtual que facilita la conexión entre mascotas necesitadas de hogar y personas interesadas en brindarles amor y cuidados. Ofrece perfiles detallados de animales disponibles, información sobre cuidados básicos y salud, citas para visitar refugios, sistemas de donaciones y foros comunitarios. Promueve la adopción responsable y el bienestar animal.",
     navItems: [
         {
-            label: "Info",
-            href: "/info",
+            label: "Veterinarias",
+            href: "/veterinarias",
         },
         {
             label: "Blog",
@@ -24,8 +24,8 @@ export const siteConfig = {
             href: "/",
         },
         {
-            label: "Info",
-            href: "/info",
+            label: "Veterinarias",
+            href: "/veterinarias",
         },
         {
             label: "Blog",
@@ -37,10 +37,6 @@ export const siteConfig = {
         },
     ],
     links: {
-        github: "https://github.com/nextui-org/nextui",
-        twitter: "https://twitter.com/getnextui",
-        docs: "https://nextui.org",
-        discord: "https://discord.gg/9b6yyZKmH4",
-        sponsor: "https://patreon.com/jrgarciadev",
+        sponsor: "/",
     },
 };
