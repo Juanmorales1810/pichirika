7e49fe66b7c517f543d952aa29ad39d7575f8dee
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-01 16:17:41 -0300

    Feat: Se creo la pagina veterinarias

---
+++ app/veterinarias/layout.tsx
@@ -0,0 +1,13 @@
+export default function PricingLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className="flex flex-col items-center justify-start w-full h-screen gap-4 pt-16">
+			<div className="flex flex-col items-center justify-center w-full h-full">
+				{children}
+			</div>
+		</section>
+	);
+}
