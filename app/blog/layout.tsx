3fd35348880de98a107d894d7006ac79190df990
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-03 23:32:16 -0300

    Feat: Creación de la pagina blogs

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,8 +4,8 @@ export default function BlogLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
+		<section className="flex flex-col items-center justify-start w-full h-screen gap-4 pt-16">
+			<div className="flex flex-col items-center justify-center w-full h-full">
 				{children}
 			</div>
 		</section>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,25 @@
-import { title } from "@/components/primitives";
+import { WhatsAppLogo } from "@/components/icons";
+import { fontMono } from "@/config/fonts";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 export default function BlogPage() {
 	return (
-		<div>
-			<h1 className={title()}>Blog</h1>
+		<div className="flex flex-col items-center gap-2">
+			<h1 className={`text-7xl font-extrabold ${fontMono.className}`}>Blogs</h1>
+			<p className="text-2xl">&#128679;Esta pagina esta en proceso&#128679;</p>
+			<p className="text-2xl">Si tenes alguna idea o conoces a algún profesional que quiera dar su testimonio háblame!</p>
+			<Button
+				className="text-md w-36 mt-4 text-zinc-950 dark:text-zinc-100 font-bold"
+				as={Link}
+				color="success"
+				variant="shadow"
+				target="_blank"
+				href={`https://wa.me/+542646216944?text=Hola%20Me%20gustaria%20aportar%20para%20la%20seccion%20de%20blogs%20das%20mas%20😊`}
+				endContent={<WhatsAppLogo className="fill-zinc-950 dark:fill-zinc-200" />}
+			>
+				WhatsApp
+			</Button>
 		</div>
 	);
 }
