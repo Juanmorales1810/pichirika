ecc7efe0ef41d34c75645106f567441ad7e6bf39
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-13 22:30:42 -0300

    Refactor: Eliminar lógica innecesaria en la página de adopción

--- app/adopt/page.tsx
+++ app/adopt/page.tsx
@@ -1,25 +1,10 @@
 import PaginationSection from "@/components/pagination";
 import { PetIcon, UserIcon } from "@/components/icons";
-import { connectMongoDB } from "@/libs/mongodb";
 import { Button } from "@nextui-org/button";
 import { fontMono } from "@/config/fonts";
-import Pet from "@/models/pichirikas";
 import Link from "next/link";
 
-export const dynamic = "force-dynamic";
-
-const getItems = async function loadMenu() {
-	await connectMongoDB();
-	const ListPets = await Pet.find();
-	return ListPets.map(product => {
-		const obj = product.toObject();
-		obj._id = obj._id.toString(); // Convierte _id a una cadena
-		return obj;
-	}); // Usa .toObject() para convertir cada producto a un objeto JavaScript simple
-}
-
 export default async function AboutPage() {
-	const Pets = await getItems();
 	return (
 		<div className="flex flex-col items-center w-full h-full min-h-[calc(100vh-64px)]">
 			<div className="flex gap-4 py-2">
@@ -28,7 +13,7 @@ export default async function AboutPage() {
 			</div>
 			<h1 className={"font-bold text-center text-6xl " + fontMono.className}>Algunos PichiriKas</h1>
 			<div className="flex flex-wrap max-w-5xl">
-				<PaginationSection Array={Pets.reverse()} />
+				<PaginationSection />
 			</div>
 		</div>
 	);
