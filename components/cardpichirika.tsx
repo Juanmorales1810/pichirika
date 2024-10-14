11e8f2a9b43e20965fa1bd39699c33c9610a4989
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-13 22:26:28 -0300

    Refactor: Modificar componente CardPichiriKa para agregar opción de esqueleto
    Se modificó el componente CardPichiriKa para agregar la opción de mostrar un esqueleto mientras se carga la imagen y los datos. Esto mejora la experiencia del usuario al proporcionar retroalimentación visual durante la carga de contenido.

--- components/cardpichirika.tsx
+++ components/cardpichirika.tsx
@@ -5,26 +5,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 
+
 interface CardPichiriKaProps {
     id: string;
     title: string;
     image: string;
     ubication: Department;
+    skeleton?: boolean;
 }
 
 export default function CardPichiriKa(props: CardPichiriKaProps) {
-    const { title, image, ubication, id } = props;
+    const { title, image, ubication, id, skeleton } = props;
+    console.log(skeleton);
+
     return (
         <article className="flex flex-col justify-center items-center gap-4 min-w-40 max-w-48 bg-lime-50 dark:bg-lime-800 rounded-2xl p-4 border-3 border-lime-700 dark:border-lime-400">
-            <header>
-                <Image className="rounded-xl aspect-square object-cover" src={image} alt={title} width={200} height={200} />
+            <header className="">
+                {skeleton ? <div className="animate-pulse h-[154px] bg-lime-700 w-full rounded-xl aspect-square object-cover"></div> : <Image className="rounded-xl aspect-square object-cover" src={image} alt={title} width={200} height={200} />}
             </header>
-            <section className="text-center">
-                <h3 className={"text-2xl font-bold " + fontMono.className}>{title}</h3>
-                <p className="font-semibold">{ubication in mappedDepartment ? mappedDepartment[ubication] : ubication}</p>
+            <section className="text-center flex flex-col gap-2 w-full">
+                {skeleton ? <div className="animate-pulse h-4 bg-lime-700 w-full rounded delay-200"></div> : <h3 className={"text-2xl font-bold " + fontMono.className}>{title}</h3>}
+                {skeleton ? <div className="animate-pulse h-4 bg-lime-700 w-full rounded delay-400"></div> : <p className="font-semibold">{ubication in mappedDepartment ? mappedDepartment[ubication] : ubication}</p>}
             </section>
-            <footer className="flex justify-center items-center gap-2">
-                <Button as={Link} href={`/adopt/${id}`} className="font-semibold" color="success">Adoptar</Button>
+            <footer className="flex justify-center items-center gap-2 w-full">
+                {skeleton ? <div className="animate-pulse h-8 bg-lime-700 w-1/2 rounded delay-600"></div> : <Button as={Link} href={`/adopt/${id}`} className="font-semibold" color="success">Adoptar</Button>}
             </footer>
         </article>
     )
