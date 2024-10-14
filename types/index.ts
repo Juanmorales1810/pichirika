e54fdd46910c6731c6bc42f97a5bd91269229d89
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-10-13 22:27:38 -0300

    Refactor: Agregar tipos de estado para el carrito y el usuario
    Se agregaron interfaces para definir los tipos de estado del carrito y del usuario. Esto permitirá un mejor manejo de la información relacionada con el carrito de compras y el usuario en el código.

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,17 @@
-import {SVGProps} from "react";
+import { SVGProps } from "react";
+import { IUserSchema } from "@/models/user";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
+    size?: number;
 };
+export interface CartState {
+    cart: UserState;
+    addToCart: (product: UserState) => void;
+    removeFromCart: (productId: number) => void;
+    clearCart: () => void;
+}
+export interface UserState {
+    user: IUserSchema | null;
+    setUser: (user: IUserSchema) => void;
+    clearUser: () => void;
+}
