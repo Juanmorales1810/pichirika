4d9d6b71291e09e29f7671f5ce7713b80f806925
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-25 12:52:47 -0300

    Feat: agregar componente RegisterPetCTA y ajustar diseño de la página de mascotas

--- src/app/adoptar/page.tsx
+++ src/app/adoptar/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Department } from "@/validations/registerPetSchema";
 import CardPichiriKa from "@/components/cardpichirika";
 import { PaginationControls } from "@/components/pagination-controls";
+import RegisterPetCTA from "@/components/register-pet-cta";
 
 interface PaginationProps {
     name: string;
@@ -47,15 +48,15 @@ export default async function PetsPage({
     const params =
         searchParams instanceof Promise ? await searchParams : searchParams;
     const currentPage = Number(params.page) || 1;
-    const itemsPerPage = 8;
+    const itemsPerPage = 7;
 
     const data = await getPets(currentPage, itemsPerPage);
     const totalPages = data?.totalItems
         ? Math.ceil(data.totalItems / itemsPerPage)
         : 0;
     return (
-        <div className="flex flex-col items-center w-full h-full min-h-[calc(100vh-64px)]">
-            <div className="flex gap-4 py-2">
+        <section className="flex flex-col items-center w-full h-full min-h-[calc(100vh)] pt-20">
+            {/* <div className="flex gap-4 py-2">
                 <Button className="font-semibold">
                     <Link
                         href="/adopt/register-pet"
@@ -74,7 +75,7 @@ export default async function PetsPage({
                         Iniciar sesión
                     </Link>
                 </Button>
-            </div>
+            </div> */}
             <h1
                 className={
                     "font-bold text-center text-6xl " + fontMono.className
@@ -85,12 +86,13 @@ export default async function PetsPage({
             <div className="flex flex-wrap max-w-5xl">
                 <div className="flex flex-col justify-center items-center w-full h-full gap-2">
                     {data?.totalItems === 0 && (
-                        <h1 className="text-2xl font-semibold">
+                        <p className="text-2xl font-semibold">
                             No hay mascotas registradas
-                        </h1>
+                        </p>
                     )}
 
                     <ul className="flex flex-wrap justify-center gap-2 py-2">
+                        <RegisterPetCTA registerUrl="/adoptar/registrar-mascota" />
                         {data?.items.map(
                             (product: PaginationProps, index: number) => (
                                 <CardPichiriKa
@@ -112,6 +114,6 @@ export default async function PetsPage({
                     )}
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
