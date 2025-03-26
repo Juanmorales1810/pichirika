e1f780eaa32cb90df3d98cabd16cc03a92dcc8c6
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-26 12:36:03 -0300

    Feat: agregar página de veterinarias cercanas con información y componentes relevantes

---
+++ src/app/veterinarias/page.tsx
@@ -0,0 +1,22 @@
+import VeterinariasCercanas from "@/components/veterinarias";
+import { fontMono } from "@/config/fonts";
+import clsx from "clsx";
+
+export default function PricingPage() {
+    return (
+        <section className="container mx-auto flex flex-col items-center w-full h-full max-w-5xl gap-2 py-20 px-2">
+            <h1
+                className={clsx(
+                    "text-5xl font-bold text-center",
+                    fontMono.className
+                )}
+            >
+                Veterinarias cercanas
+            </h1>
+            <p className="text-lg text-center hidden md:bloc">
+                Encuentra veterinarias cercanas a tu ubicación.
+            </p>
+            <VeterinariasCercanas />
+        </section>
+    );
+}
