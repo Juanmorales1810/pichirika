6f431f50d675e5446a7b7447f9b4f781e6d6d36e
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-26 12:35:06 -0300

    Feat: actualizar configuración de ESLint para incluir reglas personalizadas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript", {
+        rules: {
+            "react/no-unescaped-entities": "off",
+            "@next/next/no-page-custom-font": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    }),
 ];
 
 export default eslintConfig;
