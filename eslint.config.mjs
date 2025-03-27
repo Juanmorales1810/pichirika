37024470908d14829d3eb92c88e223ec03c70c11
Juanmorales1810 <juan1810.jm@gmail.com>
2025-03-27 11:25:49 -0300

    Refactor: corregir la extensión de configuraciones en el archivo de configuración de ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,14 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript", {
+    // Extender configuraciones
+    ...compat.extends("next/core-web-vitals"),
+    ...compat.extends("next/typescript"),
+
+    // Definir reglas personalizadas
+    {
         rules: {
             "react/no-unescaped-entities": "off",
             "@next/next/no-page-custom-font": "off",
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-vars": "off",
         },
-    }),
+    },
 ];
 
 export default eslintConfig;
