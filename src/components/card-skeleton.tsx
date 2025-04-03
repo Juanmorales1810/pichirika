a5d5005f35f63b91e1ceca40ff6fe61b39ada4ff
Juanmorales1810 <juan1810.jm@gmail.com>
2025-04-03 12:46:25 -0300

    Agregar soporte para mostrar el botón de registro de mascotas perdidas en el componente CardSkeleton

--- src/components/card-skeleton.tsx
+++ src/components/card-skeleton.tsx
@@ -1,12 +1,24 @@
 import CardPichiriKa from "@/components/cardpichirika";
 import RegisterPetCTA from "@/components/register-pet-cta";
 
-export function CardSkeleton() {
+interface CardSkeletonProps {
+    lostPet?: boolean;
+}
+
+export function CardSkeleton(props: CardSkeletonProps) {
+    const { lostPet } = props;
     return (
         <div className="flex flex-col justify-center items-center w-full h-full gap-2">
             <ul className="flex flex-wrap justify-center gap-2 py-2">
                 {/* Include the RegisterPetCTA in the skeleton for consistent layout */}
-                <RegisterPetCTA registerUrl="/adoptar/registrar-mascota" />
+                <RegisterPetCTA
+                    lostPet={lostPet}
+                    registerUrl={
+                        lostPet
+                            ? "/mascotas-perdidas/registrar-mascota-perdida"
+                            : "/adoptar/registrar-mascota"
+                    }
+                />
 
                 {/* Show skeleton cards */}
                 {Array(9)
