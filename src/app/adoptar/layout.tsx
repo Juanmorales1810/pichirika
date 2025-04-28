c9cebd8c4473d7bc375cab7a768578ec38bda1ca
Juanmorales1810 <juan1810.jm@gmail.com>
2025-04-28 10:21:23 -0300

    Agregar metadatos para la página de adopción de mascotas

--- src/app/adoptar/layout.tsx
+++ src/app/adoptar/layout.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        default: "Adopta una mascota",
+        template: `%s - ${"Adopta una mascota"}`,
+    },
+    description: "Adopta una mascota y dale un hogar",
+    keywords: "adopción, mascotas, animales",
+};
 export default function AboutLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <main className="container mx-auto">{children}</main>;
+    return <main className="">{children}</main>;
 }
